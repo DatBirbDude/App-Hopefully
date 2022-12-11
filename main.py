f882cbf3f465cd727e57a44c03fd459da3a196e7
DatBirbDude <118132310+DatBirbDude@users.noreply.github.com>
2022-12-11 13:28:20 -0500

    1.1

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import json
+from calendar import Calendar
+import calendar
+import datetime
+
 import jicson
 
 from kivy.graphics import Color, RoundedRectangle
@@ -9,6 +13,14 @@ from kivymd.app import MDApp
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.uix.card import MDCard
+
+
+class BaseScreen(Screen):
+    def contact_button_press(self):
+        self.manager.current = 'contact'
+
+    def settings_button_press(self):
+        self.manager.current = 'settings'
 
 
 class LogInScreen(Screen):
@@ -28,7 +40,8 @@ class LogInScreen(Screen):
                 self.manager.current = 'main'
 
 
-class MainScreen(Screen):
+class MainScreen(BaseScreen):
+
     screen_width = Window.width
     screen_height = Window.height
 
@@ -54,38 +67,53 @@ class MainScreen(Screen):
     def clubs_button_press(self):
         self.manager.current = 'clubs'
 
-    def contact_button_press(self):
-        self.manager.current = 'contact'
 
-    def settings_button_press(self):
-        self.manager.current = 'settings'
-
-
-class SettingsScreen(Screen):
+class SettingsScreen(BaseScreen):
     pass
 
 
-class CalendarScreen(Screen):
+class CalendarInfo(BaseScreen):
+
+    months = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June',
+              7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+
+    date = datetime.date.today()
+    year = NumericProperty(date.year)
+    month = NumericProperty(date.month)
+    day = NumericProperty(date.day)
+    month_range = calendar.monthrange(year, month)
+    weekday_offset = NumericProperty((date.weekday() + 1) % 7)
+    week_diff = 0
+
+    def week_change(self, change):
+        if int(self.day - self.weekday_offset) <= int(self.month_range[1]):
+            self.day += change * 7
+
+    def on_update(self):
+
+
+
+class CalendarScreen(CalendarInfo):
     pass
 
 
-class MonthLayout(Screen):
+class MonthLayout(CalendarInfo):
     pass
 
 
-class ListLayout(Screen):
+class ListLayout(CalendarInfo):
     pass
 
 
-class PhotosScreen(Screen):
+class PhotosScreen(BaseScreen):
     pass
 
 
-class ClubsScreen(Screen):
+class ClubsScreen(BaseScreen):
     pass
 
 
-class ContactScreen(Screen):
+class ContactScreen(BaseScreen):
     pass
 
 
