bceb4916c6fda1f4d3f17a88ad78e57d29947821
josep <vincentbarilaro@gmail.com>
2022-12-27 17:34:46 -0500

    CALENDAR SCREEN DONE!!!!!!!! IM CRACKED!!!!

--- App-Hopefully-master/main.py
+++ App-Hopefully-master/main.py
@@ -122,16 +122,16 @@ class CalendarScreen(CalendarInfo):
         CalendarInfo.month_range = calendar.monthrange(CalendarInfo.year, CalendarInfo.month)
         CalendarInfo.weekday_offset = (calendar.weekday(CalendarInfo.year, CalendarInfo.month, CalendarInfo.day) + 1) % 7
         MonthAndYearLabel.label.text = str(CalendarInfo.months[CalendarInfo.month]) + ' ' + str(CalendarInfo.year)
-        if change < 0:
-            for i in range(0, 7):
-                DayNumsLayout.day_of_weekdays[i] = str(CalendarInfo.day - CalendarInfo.weekday_offset + i + 7)
+        for i in range(0, 7):
+            DayNumsLayout.day_of_weekdays[i] = str(CalendarInfo.day - CalendarInfo.weekday_offset + i)
+            if CalendarInfo.month_range[1] >= int(DayNumsLayout.day_of_weekdays[i]) > 0:
                 DayNumsLabels.day_labels[i].text = DayNumsLayout.day_of_weekdays[i]
-        else:
-            for i in range(0, 7):
-                DayNumsLayout.day_of_weekdays[i] = str(CalendarInfo.day - CalendarInfo.weekday_offset + i)
-                DayNumsLabels.day_labels[i].text = DayNumsLayout.day_of_weekdays[i]
-        CalendarInfo.day = int(DayNumsLayout.day_of_weekdays[DayNumsLayout.is_selected.index(True)])
-        CalendarInfo.weekday_offset = (calendar.weekday(CalendarInfo.year, CalendarInfo.month, CalendarInfo.day) + 1) % 7
+            else:
+                DayNumsLabels.day_labels[i].text = ''
+            DayNumsLayout.is_selected[i] = False
+        DayNumsLayout.is_selected[CalendarInfo.weekday_offset] = True
+        ListLayout.day_nums_layout.update_canvas()
+        ListLayout.event_widgets.set_events()
         print(CalendarInfo.day)
         print(CalendarInfo.month)
         print(CalendarInfo.year)
@@ -176,84 +176,109 @@ class MonthLayout(CalendarInfo):
     pass
 
 
-class ListLayout(CalendarInfo):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.add_widget(EventWidgets())
-
-
 class EventWidgets(Widget):
     size = (280, 650)
     num_events = 0
-    event_rects = []
 
-    event_nums = []
     summaries = []
     descriptions = []
 
     event_cards = []
+    text_buffer_x = size[0] / 40
+    text_buffer_y = size[1] / 200
 
-    # I'm low-key proud of this even though it's probably wildly inefficient and doesn't work yet
-    temp = open('Calendar.json')
-    cal = json.load(temp)
-    for i in range(0, len(cal['VCALENDAR'][0]['VEVENT'])):
-        event = cal['VCALENDAR'][0]['VEVENT'][i]
-        year = int(event['DTSTART'][0:3])
-        month = int(event['DTSTART'][4:5])
-        day = int(event['DTSTART'][6:7])
-        try:
-            duration = int(event['DURATION'][1])
-        except KeyError:
-            duration = 1
-        if year == CalendarInfo.year and month == CalendarInfo.month and day + duration > CalendarInfo.day >= day:
-            # ^ There's a logic error L BOZO
-            num_events += 1
-            event_nums.append(i)
+    # I'm low-key proud of this even though it's probably wildly inefficient
+    def do_the_json(self):
+
+        self.summaries = []
+        self.descriptions = []
+        self.num_events = 0
+
+        temp = open('Calendar.json')
+        cal = json.load(temp)
+        for i in range(0, len(cal['VCALENDAR'][0]['VEVENT'])):
+            event = cal['VCALENDAR'][0]['VEVENT'][i]
+            year = int(event['DTSTART'][0:4])
+            month = int(event['DTSTART'][4:6])
+            day = int(event['DTSTART'][6:8])
             try:
-                summaries.append(event['SUMMARY;ENCODING=QUOTED-PRINTABLE'])
+                duration = int(event['DURATION'][1])
             except KeyError:
-                summaries.append('')
-            try:
-                descriptions.append(event['DESCRIPTION;ENCODING=QUOTED-PRINTABLE'])
-            except KeyError:
-                descriptions.append('')
+                duration = 1
+            if year == CalendarInfo.year and month == CalendarInfo.month and day + duration > CalendarInfo.day >= day:
+                self.num_events += 1
+                try:
+                    self.summaries.append(event['SUMMARY;ENCODING=QUOTED-PRINTABLE'])
+                except KeyError:
+                    self.summaries.append('')
+                try:
+                    self.descriptions.append(event['DESCRIPTION;ENCODING=QUOTED-PRINTABLE'])
+                except KeyError:
+                    self.descriptions.append('')
 
-    for i in range(0, num_events):
-        event_card = []
-        event_card.append(Label(text=summaries[i],
-                                size=[size[0] / 20, size[1] / 20],
-                                pos=[size[0] / 20, size[1] * (3.5/5 + i/8)]))
-        event_card.append(Label(text=descriptions[i],
-                                size=[size[0] / 20, size[1] / 20],
-                                pos=[size[0] / 20, size[1] * (3/5 + i/8)]))
-        event_cards.append(event_card)
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        with self.canvas:
-
-            Color(246 / 255, 232 / 255, 234 / 255, 1)
-
-            for i in range(0, self.num_events):
-                self.event_rects.append(RoundedRectangle(size=[self.size[0] * 9/10, self.size[1] / 10],
-                                                         pos=[self.size[0] / 20, self.size[1] * (3/5 - i/8)],
-                                                         radius=(self.height / 20, self.height / 20)))
-
+    def create_event_labels(self):
+        self.event_cards = []
+        print(len(self.event_cards))
+        for i in range(0, self.num_events):
+            event_card = [Label(text=self.summaries[i],
+                                size=[self.size[0] * 9 / 10, self.size[1] / 20],
+                                text_size=[self.size[0] * 9 / 10, self.size[1] / 20],
+                                halign='left',
+                                valign='top',
+                                font_size=self.size[0] / 25,
+                                color=(246 / 255, 232 / 255, 234 / 255, 1),
+                                pos=[self.size[0] / 20 + self.text_buffer_x,
+                                     self.size[1] * (6.7/10 - i/5) - self.text_buffer_y]),
+                          Label(text=self.descriptions[i],
+                                size=[self.size[0] * 9/10 , self.size[1] / 40],
+                                text_size=[self.size[0] * 9 / 10 - 1.5 * self.text_buffer_x, self.size[1] / 20],
+                                halign='left',
+                                valign='top',
+                                font_size=self.size[0] / 30,
+                                color=(246 / 255, 232 / 255, 234 / 255, 1),
+                                pos=[self.size[0] / 20,
+                                     self.size[1] * (3/5 - i/5) - self.text_buffer_y])]
+            self.event_cards.append(event_card)
         for i in range(0, self.num_events):
             for o in range(0, 2):
                 self.add_widget(self.event_cards[i][o])
 
+    def set_events(self):
+        self.do_the_json()
+        self.update_canvas()
+        self.create_event_labels()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.do_the_json()
+        self.update_canvas()
+        self.create_event_labels()
+
+    def update_canvas(self):
+
+        self.canvas.clear()
+        self.size = (280, 650)
+
+        with self.canvas:
+
+            for i in range(0, self.num_events):
+                Color(34 / 255, 24 / 255, 28 / 255, 1)
+
+                RoundedRectangle(size=[self.size[0] * 9/10, self.size[1] / 6],
+                                 pos=[self.size[0] / 20, self.size[1] * (5/9 - i/5)],
+                                 radius=(self.height / 20, self.height / 20))
+
+                Color(0, 0, 0, 1)  # 132/255, 220/255, 207/255, 1
+
+                Line(rounded_rectangle=[self.size[0] / 20, self.size[1] * (5/9 - i/5),
+                                        self.size[0] * 9/10, self.size[1] / 6,
+                                        self.height / 20],
+                     width=1,
+                     close=True)
 
 
-
-
-
-
-
-
-class DayNumsLayout(BoxLayout):
+class DayNumsLayout(Widget):
+    size = (280, 650)
 
     day_of_weekdays = [str(CalendarInfo.day - CalendarInfo.weekday_offset + CalendarInfo.week_diff),
                        str(CalendarInfo.day - CalendarInfo.weekday_offset + CalendarInfo.week_diff + 1),
@@ -271,9 +296,6 @@ class DayNumsLayout(BoxLayout):
                     Color(0, 0, 0, 0)]
 
     def __init__(self, **kwargs):
-        self.size_hint = [1, None]
-        self.height = Window.height / 20
-        self.pos_hint = {'center_x' : 0.5, 'center_y' : 0.5}
         super().__init__(**kwargs)
 
         with self.canvas:
@@ -284,7 +306,7 @@ class DayNumsLayout(BoxLayout):
                 else:
                     Color(0, 0, 0, 0)
 
-                Line(circle=[Window.width * (3/18 + i/9), Window.height * 76.8/100, Window.width / 20], width=1)
+                Line(circle=[self.size[0] * (3/18 + i/9), self.size[1] * 76.8/100, self.size[0] / 20], width=1)
 
     def update_canvas(self):
 
@@ -305,11 +327,11 @@ class DayNumsLayout(BoxLayout):
         if 0 < (CalendarInfo.day - CalendarInfo.weekday_offset + 6 + 7 * change) or change > 0:
             if (CalendarInfo.day - CalendarInfo.weekday_offset + 7 * change) < CalendarInfo.month_range[1] or change < 0:
                 CalendarInfo.day += 7 * change
-                if 0 > CalendarInfo.day or CalendarInfo.day > CalendarInfo.month_range[1]:
-                    try:
-                        self.is_selected[self.previously_selected_day] = False
-                    finally:
-                        self.update_canvas()
+                for i in range(0, 7):
+                    self.is_selected[i] = False
+                if CalendarInfo.month_range[0] <= CalendarInfo.day <= CalendarInfo.month_range[1]:
+                    self.is_selected[CalendarInfo.weekday_offset] = True
+                self.update_canvas()
         for i in range(0, len(self.day_of_weekdays)):
             self.day_of_weekdays[i] = str(CalendarInfo.day - CalendarInfo.weekday_offset + i)
             if CalendarInfo.month_range[1] >= int(self.day_of_weekdays[i]) > 0:
@@ -318,21 +340,34 @@ class DayNumsLayout(BoxLayout):
                 DayNumsLabels.day_labels[i].text = ''
         print(CalendarInfo.day)
 
-    previously_selected_day = CalendarInfo.weekday_offset
+        ListLayout.event_widgets.do_the_json()
+        ListLayout.event_widgets.update_canvas()
+        ListLayout.event_widgets.create_event_labels()
 
     def on_being_clicked(self, button):
+        for i in range(0, 7):
+            self.is_selected[i] = False
         if CalendarInfo.month_range[1] >= int(self.day_of_weekdays[button]) > 0:
             self.is_selected[button] = True
             CalendarInfo.day += button - CalendarInfo.weekday_offset
             CalendarInfo.weekday_offset = (calendar.weekday(CalendarInfo.year, CalendarInfo.month,
                                                             CalendarInfo.day) + 1) % 7
-            if button != self.previously_selected_day:
-                try:
-                    self.is_selected[self.previously_selected_day] = False
-                finally:
-                    self.previously_selected_day = button
             print(CalendarInfo.day)
+            ListLayout.event_widgets.do_the_json()
+            ListLayout.event_widgets.update_canvas()
+            ListLayout.event_widgets.create_event_labels()
             self.update_canvas()
+
+
+class ListLayout(CalendarInfo):
+
+    event_widgets = EventWidgets()
+    day_nums_layout = DayNumsLayout()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_widget(self.event_widgets)
+        self.add_widget(self.day_nums_layout)
 
 
 class DayNumsLabels(Widget):
@@ -421,18 +456,25 @@ class AppMaybe(MDApp):
 
         sm = ScreenManager()
 
-        sm.add_widget(LogInScreen(name='log_in'))
-        sm.add_widget(MainScreen(name='main'))
-        sm.add_widget(CalendarScreen(name='calendar'))
-        sm.add_widget(PhotosScreen(name='photos'))
-        sm.add_widget(ClubsScreen(name='clubs'))
-        sm.add_widget(ContactScreen(name='contact'))
-        sm.add_widget(SettingsScreen(name='settings'))
+        log_in_screen = LogInScreen(name='log_in')
+        main_screen = MainScreen(name='main')
+        calendar_screen = CalendarScreen(name='calendar')
+        photos_screen = PhotosScreen(name='photos')
+        clubs_screen = ClubsScreen(name='clubs')
+        contact_screen = ContactScreen(name='contact')
+        settings_screen = SettingsScreen(name='settings')
+
+        sm.add_widget(log_in_screen)
+        sm.add_widget(main_screen)
+        sm.add_widget(calendar_screen)
+        sm.add_widget(photos_screen)
+        sm.add_widget(clubs_screen)
+        sm.add_widget(contact_screen)
+        sm.add_widget(settings_screen)
         return sm
 
 
 if __name__ == '__main__':
-
     '''
     in_file = jicson.fromFile('Calendar.ics')
     with open('Calendar.json', "w") as result:
