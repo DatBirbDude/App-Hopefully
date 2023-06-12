82659704e66cf6e91b316a42e281a7ed6efeb1f6
greenturtle537 <greenturtle537@mail.com>
2023-06-12 04:53:41 +0000

    Simple servers

--- server/main.py
+++ server/main.py
@@ -1,3 +1,25 @@
-import socket
-print("Hello World!");
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
 
+hostName = "glitchtech.top"
+serverPort = 6
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/json")
+        self.end_headers()
+        self.wfile.write(bytes("Error 469: Ben detected! Request rejected. We apologize for the inconvenience.", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
