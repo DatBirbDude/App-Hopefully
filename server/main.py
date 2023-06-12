516c4899bd60cb09f4f6c60ec20a3d6269c29cf3
greenturtle537 <greenturtle537@mail.com>
2023-06-12 05:08:26 +0000

    Add server at glitchtech.top:6, try GET /supersecret

--- server/main.py
+++ server/main.py
@@ -5,15 +5,19 @@ import time
 hostName = "glitchtech.top"
 serverPort = 6
 
-class MyServer(BaseHTTPRequestHandler):
+class HopefullyServer(BaseHTTPRequestHandler):
+    
     def do_GET(self):
+        p = self.path
         self.send_response(200)
         self.send_header("Content-type", "text/json")
         self.end_headers()
-        self.wfile.write(bytes("Error 469: Ben detected! Request rejected. We apologize for the inconvenience.", "utf-8"))
+        if(p=="/supersecret"):
+            self.wfile.write(bytes("Nice work Vincent\n", "utf-8"))
+        self.wfile.write(bytes("Error 469: Ben detected! Request rejected. We apologize for the inconvenience. =" + p + "=", "utf-8"))
 
 if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
+    webServer = HTTPServer((hostName, serverPort), HopefullyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
