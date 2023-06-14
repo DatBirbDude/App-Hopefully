ba20a6975304d44879fa23725e62e218ef2f8c97
greenturtle537 <greenturtle@myself.com>
2023-06-14 14:09:08 -0400

    Add deployment ready server

--- server/main.py
+++ server/main.py
@@ -1,6 +1,5 @@
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
 import json
 from urllib.parse import urlparse
 
@@ -10,12 +9,16 @@ serverPort = 6
 def login(self, username, password):
     l = open('creds.json')
     logins = json.load(l)
+    ret = {"res": 0}
     if username in logins['users']:
         if password == logins['users'][username]['Password']:
             name = logins['users'][username]['Name']
-            self.wfile.write(bytes("Name: " + name + "\n", "utf-8"))
-        if(logins['users'][username]['Admin']):
-            self.wfile.write(bytes("You are an admin!\n", "utf-8"))
+            #self.wfile.write(bytes("Name: " + name + "\n", "utf-8"))
+            ret["res"] = 1
+            if(logins['users'][username]['Admin']):
+                #self.wfile.write(bytes("You are an admin!\n", "utf-8"))
+                ret["res"] = 2
+    self.wfile.write(bytes(json.dumps(ret)))
     l.close()
     return
 
@@ -34,7 +37,7 @@ class HopefullyServer(BaseHTTPRequestHandler):
             username = query_components["username"]
             password = query_components["password"]
             login(self, username, password)
-            self.wfile.write(bytes("Lol your login is " + username + ":" +  password + "\n", "utf-8"))
+            #self.wfile.write(bytes("Lol your login is " + username + ":" +  password + "\n", "utf-8"))
         if(p[0]=="/supersecret"):
             self.wfile.write(bytes("Nice work Vincent\n", "utf-8"))
         self.wfile.write(bytes("Error 469: Ben detected! Request rejected. We apologize for the inconvenience. =" + p[0] + "=!" + query + "!", "utf-8"))
